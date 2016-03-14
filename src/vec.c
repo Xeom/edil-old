31782c7bdd471065aeeb84d4920ea42fea5ffe1a
Xeom <fwharf.fw@googlemail.com>
2016-03-14 06:26:10 +0000

    Commented vec.c

--- src/vec.c
+++ src/vec.c
@@ -20,8 +20,16 @@ static int vec_realloc(vec *v)
 {
     v->data = realloc(v->data, v->capacity);
 
+    /* Check allocs :D */
     if (v->data == NULL)
+    {
+        /* Set the capacity and length to 0 to try and prevent *
+         * anything segfaulting off of this                    */
+        v->length   = 0;
+        v->capacity = 0;
+
         ERR(NO_MEMORY, -1);
+    }
 
     ERR(OK, 0);
 }
@@ -32,13 +40,22 @@ static int vec_resize_bigger(vec *v)
 
     length = v->length;
 
+    /* If we have enough capacity we can just return */
     if (v->capacity >= length)
         ERR(OK, 0);
 
+    /* If due to errors, we get a capacity of 0, *
+     * set it to the minimum allowable value     */
+    if (v->capacity == 0)
+        v->capacity = v->width;
+
+    /* If we don't have capacity, double it, and if  *
+     * that STILL isn't enough, keep doing it.       */
     do
         v->capacity <<= 1;
     while (v->capacity < length);
 
+    /* Now that we've changed v->capacity, we gotta realloc */
     return vec_realloc(v);
 }
 
@@ -49,15 +66,20 @@ static int vec_resize_smaller(vec *v)
     length = v->length;
     width  = v->width;
 
+    /* If we are using over one quarter capacity, or *
+     * our capacity is at minimum already, return    */
     if (length <= v->capacity >> 2 ||
         width  == v->capacity)
         ERR(OK, 0);
 
+    /* If we need to shrink, halve the capacity, and if *
+     * we still need shrink, keep doing it              */
     do
         v->capacity >>= 1;
     while (length > v->capacity &&
            width  < v->capacity);
 
+    /* Now that we've changed v->capacity, we gotta realloc */
     return vec_realloc(v);
 }
 
@@ -75,9 +97,11 @@ vec *vec_init(size_t width)
     if (rtn == NULL)
         ERR(NO_MEMORY, NULL);
 
+    /* Data must be null so realloc will malloc */
     rtn->data     = NULL;
     rtn->width    = width;
     rtn->length   = 0;
+    /* Minium capacity is one width */
     rtn->capacity = width;
 
     /* We set our capacity to the vector's     *
@@ -107,6 +131,7 @@ void *vec_item(vec *v, size_t index)
     /* Calculate and copy to offset */
     offset = index * v->width;
 
+    /* Check that index is in bounds */
     if (offset + v->width > v->length)
         ERR(INVALID_INDEX, NULL);
 
@@ -120,16 +145,21 @@ int vec_delete(vec *v, size_t index, size_t n)
     if (v == NULL)
         ERR(NULL_VEC, -1);
 
+    /* Calculate the offset of the startbyte of the     *
+     * deletion, and the amount of bytes we will delete */
     offset = index * v->width;
     amount =     n * v->width;
 
+    /* Check that the range to delete isn't out of bounds */
     if (offset + amount > v->length)
         ERR(INVALID_INDEX, -1);
 
+    /* Move memory beyond offset backwards by amount. */
     memmove(addptr(v->data, offset),
             addptr(v->data, offset + amount),
             v->length - offset);
 
+    /* Modify length and resize */
     v->length -= amount;
 
     return vec_resize_smaller(v);
@@ -145,23 +175,31 @@ int vec_insert(vec *v, size_t index, size_t n, const void *new)
     if (new == NULL)
         ERR(NULL_VALUE, -1);
 
+    /* Calculate the offset of the startbyte of the       *
+     * insertion, and the amount of bytes we will insert, *
+     * and the size of the region displaced forward.      */
     offset = index * v->width;
     amount =     n * v->width;
     displaced = v->length - offset;
 
+    /* Make sure we're in bounds */
     if (offset > v->length)
         ERR(INVALID_INDEX, -1);
 
+    /* We adjust length and realloc before moving anything */
     v->length += amount;
 
     if (vec_resize_bigger(v) == -1)
         return -1;
 
+    /* If any data is displaced, we move it forward to make *
+     * room for inserted data.                              */
     if (displaced)
         memmove(addptr(v->data, offset + amount),
                 addptr(v->data, offset),
                 displaced);
 
+    /* We insert the new data */
     memmove(addptr(v->data, offset),
             new, amount);
 
@@ -173,6 +211,8 @@ size_t vec_len(vec *v)
     if (v == NULL)
         ERR(NULL_VEC, 0);
 
+    /* To get the number of items, divide *
+     * length in bytes by width in bytes  */
     ERR(OK, v->length / v->width);
 }
 
@@ -187,20 +227,26 @@ size_t vec_find(vec *v, const void *item)
     if (item == NULL)
         ERR(NULL_VALUE, INVALID_INDEX);
 
+    /* Get the first item of the list, and *
+     * the one just past the end (invalid) */
     width  = v->width;
     cmp    = v->data;
     last   = addptr(cmp, v->length);
 
+
     index  = 0;
     while (cmp != last)
     {
+        /* If there's a match, return the index */
         if (memcmp(cmp, item, width) == 0)
             ERR(OK, index);
 
+        /* Increment cmp by width, and index by 1 */
         cmp = addptr(cmp, width);
         ++index;
     }
 
+    /* No match */
     ERR(INVALID_VALUE, INVALID_INDEX);
 }
 
@@ -215,20 +261,26 @@ size_t vec_rfind(vec *v, const void *item)
     if (item == NULL)
         ERR(NULL_VALUE, INVALID_INDEX);
 
+    /* Get the first item of the list, and *
+     * the one just past the end (invalid) */
     width  = v->width;
     first  = v->data;
     cmp    = addptr(first, v->length);
 
+    /* While we're not at the start */
     index  = vec_len(v);
     while (cmp > first)
     {
+        /* Decrement cmp by width and index by 1 */
         index--;
         cmp = subptr(cmp, width);
 
+        /* If there's a match, return the index */
         if (memcmp(cmp, item, width) == 0)
             ERR(OK, index);
     }
 
+    /* No match */
     ERR(INVALID_VALUE, INVALID_INDEX);
 }
 
@@ -252,6 +304,7 @@ const char *vec_err_str(void)
     if (vecerr == E_VEC_INVALID_WIDTH)
         return "E_VEC_INVALID_WIDTH";
 
+    /* Great error. */
     if (vecerr == E_VEC_OK)
         return "E_VEC_OK";
 
