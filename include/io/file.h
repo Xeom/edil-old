1b9ecd9c72f0f85ddd1251ff03ab3a7caa0b5179
Xeom <fwharf.fw@googlemail.com>
2016-03-29 14:49:11 +0100

    more io/file functions

--- include/io/file.h
+++ include/io/file.h
@@ -2,6 +2,12 @@
 # define IO_FILE_H
 # include "head.h"
 
+# include "container/vec.h"
+
+int file_dump_vec(vec *v, FILE *stream);
+
 int file_dump_buffer(buffer *b, FILE *stream);
+
+int file_read_buffer(buffer *b, FILE *stream);
 
 #endif /* IO_FILE_H */
--- src/io/file.c
+++ src/io/file.c
@@ -7,9 +7,18 @@
 #endif
 
 #include "err.h"
-#include "container/vec.h"
 #include "buffer/core.h"
 #include "io/file.h"
+
+int file_dump_vec(vec *v, FILE *stream)
+{
+    char *str;
+    str = vec_item(v, 0);
+
+    fwrite(str, 1, vec_len(v), stream);
+
+    return 0;
+}
 
 int file_dump_buffer(buffer *b, FILE *stream)
 {
@@ -23,10 +32,50 @@ int file_dump_buffer(buffer *b, FILE *stream)
         line *l;
 
         l = buffer_get_line(b, currln++);
-        
-        fputs(vec_item((vec *)l, 0), stream);
+
+        file_dump_vec((vec *)l, stream);
     }
 
     return 0;
 }
 
+static int file_read_buffer_line(buffer *b, FILE *stream)
+{
+    char val;
+    int chr;
+    size_t len;
+    line *l;
+
+    len = buffer_len(b);
+    l = buffer_insert(b, len);
+
+    while (1)
+    {
+        chr = fgetc(stream);
+
+        if (chr == EOF)
+            return EOF;
+
+        val = (char)chr;
+
+        vec_insert_end((vec *)l, 1, &val);
+
+        if (chr == '\n')
+            return 0;
+    }
+}
+
+int file_read_buffer(buffer *b, FILE *stream)
+{
+    int val;
+    for (;;)
+    {
+        val = file_read_buffer_line(b, stream);
+
+        if (val == EOF)
+            return 0;
+
+        if (val != 0)
+            return -1;
+    }
+}
