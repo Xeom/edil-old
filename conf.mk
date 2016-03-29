394694d2a2efd433ffcdd70299f65f155fbfa6a5
Xeom <fwharf.fw@googlemail.com>
2016-03-29 14:46:14 +0100

    Updated compile

--- conf.mk
+++ conf.mk
@@ -16,7 +16,7 @@ INC=include
 WARNINGS=all extra pedantic missing-prototypes fatal-errors format conversion declaration-after-statement
 LINKS=ncurses
 
-SRC_NAMES=line err cursor wintree wincont chunk textcont hook io callback \
+SRC_NAMES=err wintree wincont hook io callback \
  buffer/chunk buffer/core buffer/line \
  container/table container/vec \
  ui/core ui/sbar ui/win ui/face ui/util \
