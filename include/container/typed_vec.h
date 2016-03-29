a8b98c9462cdd52a50bfacbec2cf2039f6d15599
Xeom <fwharf.fw@googlemail.com>
2016-03-29 05:31:53 +0100

    Fixed up typed_vec, head.h

--- include/container/typed_vec.h
+++ include/container/typed_vec.h
@@ -1,4 +1,5 @@
 #include "vec.h"
+#include "err.h"
 
 #ifndef VEC_TYPED_TYPE
 # define VEC_TYPED_TYPE void *
@@ -9,75 +10,75 @@
 # error "typed_vec included with no VEC_TYPED_NAME defined"
 #endif
 
-#define VEC_TYPED_VNAME vec_ ## VEC_TYPED_NAME;
-#define VEC_TYPED_FNAME(name) VEC_TYPED_VNAME ## _ ## name
+#define VEC_TYPED_VNAME CONCAT(vec_,VEC_TYPED_NAME)
+#define VEC_TYPED_FNAME(name) CONCAT(VEC_TYPED_VNAME, _ ## name)
 
 typedef vec VEC_TYPED_VNAME;
 
-static int VEC_TYPED_FNAME(create)(void *mem)
+static inline VEC_TYPED_VNAME *VEC_TYPED_FNAME(create)(void *mem)
 {
     return vec_create(sizeof(VEC_TYPED_TYPE), mem);
 }
 
-static VEC_TYPED_VNAME *VEC_TYPED_FNAME(init)(void)
+static inline VEC_TYPED_VNAME *VEC_TYPED_FNAME(init)(void)
 {
     return vec_init(sizeof(VEC_TYPED_TYPE));
 }
 
-static void *VEC_TYPED_FNAME(free)(VEC_TYPED_VNAME *v)
+static inline void VEC_TYPED_FNAME(free)(VEC_TYPED_VNAME *v)
 {
     vec_free(v);
 }
 
-static VEC_TYPED_TYPE *VEC_TYPED_VNAME ## _item(VEC_TYPED_VNAME *v, size_t index)
+static inline VEC_TYPED_TYPE *VEC_TYPED_FNAME(item)(VEC_TYPED_VNAME *v, size_t index)
 {
     return vec_item(v, index);
 }
 
-static int VEC_TYPED_FNAME(delete)(VEC_TYPED_VNAME *v, size_t index, size_t n)
+static inline int VEC_TYPED_FNAME(delete)(VEC_TYPED_VNAME *v, size_t index, size_t n)
 {
     return vec_delete(v, index, n);
 }
 
-static int VEC_TYPED_FNAME(insert)(VEC_TYPED_VNAME *v, size_t index, size_t n, const VEC_TYPED_TYPE *new)
+static inline int VEC_TYPED_FNAME(insert)(VEC_TYPED_VNAME *v, size_t index, size_t n, VEC_TYPED_TYPE *new)
 {
     return vec_insert(v, index, n, new);
 }
 
-static size_t VEC_TYPED_FNAME(len)(VEC_TYPED_VNAME *v)
+static inline size_t VEC_TYPED_FNAME(len)(VEC_TYPED_VNAME *v)
 {
     return vec_len(v);
 }
 
-static size_t VEC_TYPED_FNAME(find)(VEC_TYPED_VNAME *v, const VEC_TYPED_TYPE *new)
+static inline size_t VEC_TYPED_FNAME(find)(VEC_TYPED_VNAME *v, VEC_TYPED_TYPE *item)
 {
     return vec_find(v, item);
 }
 
-static size_t VEC_TYPED_FNAME(rfind)(VEC_TYPED_VNAME *v, const VEC_TYPED_TYPE *new)
+static inline size_t VEC_TYPED_FNAME(rfind)(VEC_TYPED_VNAME *v, const VEC_TYPED_TYPE *item)
 {
     return vec_rfind(v, item);
 }
 
-static VEC_TYPED_VNAME *VEC_TYPED_FNAME(cut)(VEC_TYPED_VNAME *v, size_t index, size_t n)
+static inline VEC_TYPED_VNAME *VEC_TYPED_FNAME(cut)(VEC_TYPED_VNAME *v, size_t index, size_t n)
 {
     return vec_cut(v, index, n);
 }
 
-static VEC_TYPED_TYPE VEC_TYPED_FNAME(get)(VEC_TYPED_VNAME *v, size_t index)
-{
-    VEC_TYPED_TYPE *rtnptr;
+#ifdef VEC_TYPED_GETSET
+# undef VEC_TYPED_GETSET
+  static inline VEC_TYPED_TYPE VEC_TYPED_FNAME(get)(VEC_TYPED_VNAME *v, size_t index)
+  {
+      VEC_TYPED_TYPE *rtnptr;
+      rtnptr = VEC_TYPED_FNAME(item)(v, index);
+      return *rtnptr;
+  }
 
-    TRACE_PTR(rtnptr = VEC_TYPED_FNAME(item)(v, index),
-              return 0);
-
-    return *rtnptr;
-}
-
-static int VEC_TYPED_FNAME(set)(VEC_TYPED_VNAME *v, size_t index, VEC_TYPED_TYPE value)
-{
-    return vec_insert(v, index, 1, &value);
-}
+  static inline int VEC_TYPED_FNAME(set)(VEC_TYPED_VNAME *v, size_t index, VEC_TYPED_TYPE value)
+  {
+      return vec_insert(v, index, 1, &value);
+  }
+#endif
 
 #undef VEC_TYPED_TYPE
 #undef VEC_TYPED_NAME
---
+++ include/head.h
@@ -0,0 +1,17 @@
+#ifndef HEAD_H
+# define HEAD_H
+
+typedef unsigned int uint;
+typedef unsigned char uchar;
+typedef unsigned short ushort;
+
+#define REALLY_STRIFY(a) #a
+#define STRIFY(a) REALLY_STRIFY()
+
+#define REALLY_CONCAT(a, b) a ## b
+#define CONCAT(a, b) REALLY_CONCAT(a, b)
+
+#define MAX(a, b) a > b ? a : b
+#define MIN(a, b) a > b ? b : a
+
+#endif /* HEAD_H */
