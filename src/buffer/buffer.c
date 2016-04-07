026f06366166bfdc2dd5d87683d12ecf39c0feff
Xeom <fwharf.fw@googlemail.com>
2016-04-07 11:28:24 +0100

    Added errors to buffer.c

--- src/buffer/buffer.c
+++ src/buffer/buffer.c
@@ -2,6 +2,7 @@
 
 #include "buffer/chunk.h"
 #include "buffer/line.h"
+#include "err.h"
 
 #define VEC_TYPED_GETSET
 #define VEC_TYPED_TYPE line *
@@ -12,8 +13,13 @@
 
 struct buffer_s
 {
-    chunk    *currchunk;
-    char     *name;
+    chunk *currchunk; /* Linked list (most recent link) of chunks */
+
+    struct
+    {
+        unsigned int modified:1;
+        unsigned int readonly:1;
+    } flags;
 };
 
 hook_add(buffer_line_on_delete, 2);
@@ -34,11 +40,12 @@ buffer *buffer_init(void)
 {
     buffer *rtn;
 
-    rtn = malloc(sizeof(buffer));
+    ASSERT_PTR(rtn = malloc(sizeof(buffer)),
+               terminal, return NULL);
 
     /* We need to initialize a new chunk for the buffer as well */
-    rtn->currchunk = buffer_chunk_init(rtn);
-    rtn->name = NULL;
+    TRACE_PTR(rtn->currchunk = buffer_chunk_init(rtn),
+              return NULL);
 
     hook_call(buffer_on_create, &rtn);
 
@@ -52,21 +59,19 @@ void buffer_free(buffer *b)
     /* Free the chunks (and lines) */
     buffer_chunk_free(b->currchunk);
 
-    /* Free the buffer's name */
-    if (b->name)
-        free(b->name);
-
     /* Free the actual buffer */
     free(b);
 }
 
 static inline chunk *buffer_get_containing_chunk(buffer *b, lineno ln)
 {
-    /* Setting the currchunk to the chunk we last visited makes for  *
-     * much faster accesses, assuming accesses are fairly localized. */
-    b->currchunk = buffer_chunk_get_containing(b->currchunk, ln);
+    chunk *c;
 
-    return b->currchunk;
+    c            = buffer_chunk_get_containing(b->currchunk, ln);
+    /* This variable acts like a cache to speed up the linked list */
+    b->currchunk = c;
+
+    return c;
 }
 
 int buffer_insert(buffer *b, lineno ln)
@@ -75,11 +80,16 @@ int buffer_insert(buffer *b, lineno ln)
     lineno offset;
 
     /* Get the chunk, and depth into that chunk of where we want to insert. */
-    c      = buffer_get_containing_chunk(b, ln);
-    offset = buffer_chunk_lineno_to_offset(c, ln);
+    TRACE_PTR(c      = buffer_get_containing_chunk(b, ln),
+              return -1);
+    TRACE_IND(offset = buffer_chunk_lineno_to_offset(c, ln),
+              return -1);
 
     /* Aaaaand we've abstracted over the rest cos we're useless. */
-    return buffer_chunk_insert_line(c, offset);
+    TRACE_INT(buffer_chunk_insert_line(c, offset),
+              return -1);
+
+    return 0;
 }
 
 int buffer_delete(buffer *b, lineno ln)
@@ -88,11 +98,14 @@ int buffer_delete(buffer *b, lineno ln)
     lineno offset;
 
     /* Get the chunk, and depth into that chunk of where we want to delete. */
-    c      = buffer_get_containing_chunk(b, ln);
-    offset = buffer_chunk_lineno_to_offset(c, ln);
+    TRACE_PTR(c      = buffer_get_containing_chunk(b, ln),
+              return -1);
+    TRACE_IND(offset = buffer_chunk_lineno_to_offset(c, ln),
+              return -1);
 
     /* Bla */
-    buffer_chunk_delete_line(c, offset);
+    TRACE_INT(buffer_chunk_delete_line(c, offset),
+              return -1);
 
     return 0;
 }
@@ -104,10 +117,13 @@ vec *buffer_get_line(buffer *b, lineno ln)
     vec  *rtn;
 
     /* Get the chunk, and depth into that chunk of where we want to get. */
-    c      = buffer_get_containing_chunk(b, ln);
-    offset = buffer_chunk_lineno_to_offset(c, ln);
+    TRACE_PTR(c      = buffer_get_containing_chunk(b, ln),
+              return NULL);
+    TRACE_IND(offset = buffer_chunk_lineno_to_offset(c, ln),
+              return NULL);
 
-    rtn    = buffer_chunk_get_line(c, offset);
+    TRACE_INT(rtn    = buffer_chunk_get_line(c, offset),
+              return NULL);
 
     return rtn;
 }
@@ -118,10 +134,15 @@ int buffer_set_line(buffer *b, lineno ln, vec *v)
     lineno offset;
 
     /* Get the chunk, and depth into that chunk of where we want to get. */
-    c      = buffer_get_containing_chunk(b, ln);
-    offset = buffer_chunk_lineno_to_offset(c, ln);
+    TRACE_PTR(c      = buffer_get_containing_chunk(b, ln),
+              return -1);
+    TRACE_IND(offset = buffer_chunk_lineno_to_offset(c, ln),
+              return -1);
 
-    return buffer_chunk_set_line(c, offset, v);
+    TRACE_INT(buffer_chunk_set_line(c, offset, v),
+              return -1);
+
+    return 0;
 }
 
 lineno buffer_len(buffer *b)
