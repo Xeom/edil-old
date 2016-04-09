1203f8010e447dd79623284195b0e34fecd225d1
Xeom <fwharf.fw@googlemail.com>
2016-04-09 11:02:30 +0100

    Added supression file

--- python/init.sh
+++ python/init.sh
@@ -5,7 +5,7 @@ make -C .. lib &&
         if [[ ! -f stfu-python.supp ]]; then
            curl https://svn.python.org/projects/python/trunk/Misc/valgrind-python.supp > stfu-python.supp
         fi
-        valgrind --suppressions=stfu-python.supp python3 __init__.py 2>tmp
+        valgrind --leak-check=full --show-leak-kinds=all --suppressions=stfu-python.supp --suppressions=stfu.supp python3 __init__.py 2>tmp
         cat tmp
     else
         python3 __init__.py
