d4e0e52dacb38ae3dda4cf89787f17b082c3c523
Xeom <fwharf.fw@googlemail.com>
2016-04-10 20:55:17 +0100

    Misc changes to cutil/init.sh

--- python/cutil.py
+++ python/cutil.py
@@ -1,5 +1,37 @@
+import os
+
 import ctypes
+from shared import lib as so
 
 def isnull(ptr):
     return ctypes.cast(ptr, ctypes.c_void_p).value == None
+
+def str2char(s):
+    if isinstance(s, str):
+        return ctypes.c_char_p(s.encode("ascii"))
+
+    else:
+        return ctypes.c_char_p(s)
+
+so.fopen.argtypes = [ctypes.c_char_p, ctypes.c_char_p]
+so.fopen.restype  = ctypes.c_void_p
+
+so.fclose.argtypes = [ctypes.c_void_p]
+so.fclose.restype  = ctypes.c_int
+
+def fptr_open(path, mode):
+    path = os.path.abspath(path)
+
+    if not os.path.isfile(path):
+        raise IOError("File not found '{}'".format(path))
+
+    fptr = so.fopen(str2char(path), str2char(mode))
+
+    if isnull(fptr):
+        raise Exception("No.")
+
+    return fptr
+
+def fptr_close(fptr):
+    so.fclose(fptr)
 
--- python/init.sh
+++ python/init.sh
@@ -1,17 +1,20 @@
 
 #!/bin/bash
 
+echo > tmp
+
 make -C .. lib &&
     if [[ $* == *debug* ]]; then
         if [[ ! -f stfu-python.supp ]]; then
            curl https://svn.python.org/projects/python/trunk/Misc/valgrind-python.supp > stfu-python.supp
         fi
 		if [[ $* == *full* ]]; then
-	        valgrind --leak-check=full --show-leak-kinds=all --suppressions=stfu-python.supp --suppressions=stfu.supp python3 __init__.py 2>tmp
+	        valgrind --leak-check=full --show-leak-kinds=all --suppressions=stfu-python.supp --suppressions=stfu.supp python3 -B __init__.py 2>tmp
 		else
-	        valgrind --leak-check=full --show-leak-kinds=possible,indirect,definite --suppressions=stfu-python.supp --suppressions=stfu.supp python3 __init__.py 2>tmp
+	        valgrind --leak-check=full --show-leak-kinds=possible,indirect,definite --suppressions=stfu-python.supp --suppressions=stfu.supp python3 -B __init__.py 2>tmp
 		fi
-        cat tmp
     else
-        python3 __init__.py 2>tmp
+        python3 -B __init__.py 2>tmp
     fi
+
+cat tmp
