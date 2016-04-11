08a5951abd25daf22b131d071710d3f1a593a6a2
Xeom <fwharf.fw@googlemail.com>
2016-04-11 17:18:45 +0100

    Major re-do of test program

--- python/__init__.py
+++ python/__init__.py
@@ -1,110 +1,84 @@
 #!/bin/python
 
-import systems.ui
-import systems.windows
-import systems.hook
-import systems.io
-import c
+import core.ui
+import core.windows
+import core.hook
+import core.io
 import time
 import ctypes
 import shared
 import signal
-import c.lib
-
-def append(b, text):
-    c.buffer.insert(b, 0)
-
-    l = c.vec.str2vec(text)
-
-    c.buffer.set_line(b, 0, l.struct)
+import symbols
+from editor.cursor.point import Point
 
 
-@systems.ui.hooks.win.content.draw_pre(100)
-def handle_content_draw_pre(w, b):
-    append(b, b"I was drawn ^-^")
+#@core.ui.hooks.win.frame.draw_pre(100)
+#def handle_frame_draw_pre(w):
+#    w.caption = str(len(w.buffer))
+#
+#@core.ui.hooks.win.content.draw_pre(100)
+#def handle_content_draw_pre(w, b):
+#    b.push("I was drawn :D")
+#
+#@core.windows.hooks.split(100)
+#def handle_split(wb, wbaby):
+#    b = wbaby.buffer
+#
+#@core.windows.hooks.create(100)
+#def handle_created(w):
+#    b = w.buffer
 
-    if not w.caption:
-        w.caption = "0"
+core.windows.initsys()
+print("HI")
+core.ui.initsys()
+core.io.initsys()
 
-    w.caption = str(int(w.caption) + 1)
+core.ui.refresh()
 
-@systems.windows.hooks.split(100)
-def handle_split(wb, wbaby):
-    b = wbaby.buffer
-    append(b, b"I was created in a split :D")
-
-@systems.windows.hooks.create(100)
-def handle_created(w):
-    b = w.buffer
-    append(b, b"Yay I exist!!")
-
-systems.windows.initsys()
-systems.ui.initsys()
-systems.io.initsys()
-
-systems.ui.refresh()
-
-
-
-import c.buffer
-
-
-"""
-@systems.windows.hooks.resizex(100)
-@systems.windows.hooks.resizey(100)
-def handle_resize(w, x, y):
-    sizex = x.value
-    sizey = y.value
-
-    w.sidebar = "Cunt"
-
-    w.caption = "{}x{}".format(sizex, sizey)
-
-@systems.io.hooks.keypress(1)
-def handle_hook(k):
-    c.ui.sbar.set(bytes(k))
-    c.lib.refresh()
-"""
-
-
+p = Point(core.windows.get_selected().buffer)
 
 while True:
-    char = c.lib.getch()
+    char = symbols.lib.getch()
 
-    if char == c.io.key_resize:
-        systems.ui.resize()
+    if char == symbols.io.key_resize:
+        core.ui.resize()
 
-    if char == ord('w'):
-        systems.windows.get_selected().split(systems.windows.direction.up)
+    elif char == ord('w'):
+        core.windows.get_selected().split(core.windows.direction.up)
 
-    if char == ord('s'):
-        systems.windows.get_selected().split(systems.windows.direction.down)
+    elif char == ord('s'):
+        core.windows.get_selected().split(core.windows.direction.down)
 
-    if char == ord('a'):
-        systems.windows.get_selected().split(systems.windows.direction.left)
+    elif char == ord('a'):
+        core.windows.get_selected().split(core.windows.direction.left)
 
-    if char == ord('d'):
-        systems.windows.get_selected().split(systems.windows.direction.right)
+    elif char == ord('d'):
+        core.windows.get_selected().split(core.windows.direction.right)
 
-    if char == ord('e'):
-        systems.windows.get_selected().next().select()
+    elif char == ord('l'):
+        core.windows.get_selected().buffer.read("__init__.py")
 
-    if char == ord('c'):
-        systems.windows.get_selected().delete()
+    elif char == ord('e'):
+        core.windows.get_selected().next().select()
 
-    if char == ord('x'):
+    elif char == ord('c'):
+        core.windows.get_selected().delete()
+
+    elif char == ord('x'):
         break
 
-    if char == ord('u'):
-        systems.windows.get_selected().parent.select()
+    elif char == ord('u'):
+        core.windows.get_selected().parent.select()
+
+    elif char == 10:
+        p.enter()
 
     else:
-        c.io.handle_chr(char)
+        p.insert(chr(char))
+#        symbols.io.handle_chr(char)
 
-#k    systems.windows.Window(c.win.select.get()).sidebar = "Hiiii"
-    systems.ui.refresh()
+#k    core.windows.Window(symbols.win.select.get()).sidebar = "Hiiii"
+    core.ui.refresh()
 
-systems.ui.killsys()
+core.ui.killsys()
 
-import gc
-gc.collect()
