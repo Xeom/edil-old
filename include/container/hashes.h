eaf75b8b8afc31ccd82c29b862a0dd0ff06f0c3e
Xeom <fwharf.fw@googlemail.com>
2016-04-14 23:56:30 +0100

    Added hashes.[ch] Only contain hashes_str(_trans) at the moment

---
+++ include/container/hashes.h
@@ -0,0 +1,9 @@
+#if !defined(CONTAINER_HASHES_H)
+# define CONTAINER_HASHES_H
+# include "head.h"
+
+hash hashes_str(char *str);
+
+hash hashes_str_trans(char *str);
+
+#endif /* CONTAINER_HASHES_H */
