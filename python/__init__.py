4c3d7b72eb93bfef7450c685894220e167fc9ad6
Xeom <fwharf.fw@googlemail.com>
2016-04-20 22:23:54 +0100

    Updated python test program

--- python/__init__.py
+++ python/__init__.py
@@ -4,13 +4,16 @@ import core.ui
 import core.windows
 import core.hook
 import core.key
+import core.keymap
 import ctypes
 import shared
 import symbols
+import signal
 
 import editor.uiupdates
 from editor.cursor.point import Point
-
+from core.key import Key
+from core.keymap import Keymap
 
 #@core.ui.hooks.win.frame.draw_pre(100)
 #def handle_frame_draw_pre(w):
@@ -27,15 +30,18 @@ from editor.cursor.point import Point
 #@core.windows.hooks.create(100)
 #def handle_created(w):
 #    b = w.buffer
+symbols.hook.initsys()
 
 core.windows.initsys()
 core.ui.initsys()
 core.key.initsys()
 
 core.ui.refresh()
-alive = True
+
 @core.key.hooks.key(100)
 def handle_keypress(key):
+    km.press(key)
+    return
     char = str(key)
 
     if char == '^W':
@@ -56,7 +62,7 @@ def handle_keypress(key):
     elif char == '^E':
         core.windows.get_selected().next().select()
 
-    elif char == '^C':
+    elif char == '^F':
         core.windows.get_selected().delete()
 
     elif char == '^X':
@@ -75,8 +81,36 @@ def handle_keypress(key):
     else:
         p.insert(char)
 
-p = Point(core.windows.get_selected().buffer)
+p  = Point(core.windows.get_selected().buffer)
+km = core.keymap.Keymap()
+km.struct
 
+alive = True
+
+def death_by_gentle_suffocation( signum, frame):
+    global alive
+    alive = False
+
+signal.signal(signal.SIGINT, death_by_gentle_suffocation)
+signal.signal(signal.SIGTERM, death_by_gentle_suffocation)
+
+@km.add(Key("W", con=True), Key("UP"))
+def winsplitup(keys):
+    core.windows.get_selected().split(core.windows.direction.up)
+
+@km.add(Key("W", con=True), Key("DOWN"))
+def winsplitdown(keys):
+    core.windows.get_selected().split(core.windows.direction.down)
+
+@km.add(Key("W", con=True), Key("LEFT"))
+def winsplitleft(keys):
+    core.windows.get_selected().split(core.windows.direction.left)
+
+@km.add(Key("W", con=True), Key("RIGHT"))
+def winsplitright(keys):
+    core.windows.get_selected().split(core.windows.direction.right)
+
+    
 while alive:
     char = symbols.lib.getch()
 
@@ -92,3 +126,4 @@ while alive:
 
 core.ui.killsys()
 
+#symbols.hook.killsys()
--- python/init.sh
+++ python/init.sh
@@ -8,11 +8,11 @@ make -C .. lib &&
         if [[ ! -f stfu-python.supp ]]; then
            curl https://svn.python.org/projects/python/trunk/Misc/valgrind-python.supp > stfu-python.supp
         fi
-		if [[ $* == *full* ]]; then
-	        valgrind --leak-check=full --show-leak-kinds=all --suppressions=stfu-python.supp --suppressions=stfu.supp python3 -B __init__.py 2>tmp
-		else
-	        valgrind --leak-check=full --show-leak-kinds=possible,indirect,definite --suppressions=stfu-python.supp --suppressions=stfu.supp python3 -B __init__.py 2>tmp
-		fi
+        if [[ $* == *full* ]]; then
+            valgrind --track-origins=yes --leak-check=full --show-leak-kinds=all --suppressions=stfu-python.supp --suppressions=stfu.supp python3 -B __init__.py 2>tmp
+        else
+            valgrind --track-origins=yes --leak-check=full --show-leak-kinds=possible,indirect,definite --suppressions=stfu-python.supp --suppressions=stfu.supp python3 -B __init__.py 2>tmp
+        fi
     else
         python3 -B __init__.py 2>tmp
     fi
