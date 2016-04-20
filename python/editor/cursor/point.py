1e01be8b6aa72a523563b09c974616198da71323
Xeom <fwharf.fw@googlemail.com>
2016-04-20 06:54:13 +0100

    Updated point.py. Will likely be replaced when attach.c is finished though.

--- python/editor/cursor/point.py
+++ python/editor/cursor/point.py
@@ -2,31 +2,43 @@ import core.buffer
 
 from symbols.vec import str2vec
 
+import sys
+
+
 class Point:
     def __init__(self, buffer):
         self.buffer = buffer
-        self.cn     = 0
-        self.ln     = 0
 
-        @core.buffer.hooks.line.delete_pre(500)
+        if len(self.buffer) == 0:
+            self.buffer.insert(0)
+
+        self.cn = 0
+        self.ln = 0
+
+        @core.buffer.hooks.line.delete_post(500)
         def handle_line_delete(b, ln, v):
             if self.ln >= ln.value and self.buffer == b:
-                    self.ln += 1
+                self.ln -= 1
+
+                self.correct_linepos()
+                self.correct_colpos()
 
         self.handle_line_delete = handle_line_delete
 
-        @core.buffer.hooks.line.change_pre(500)
+        @core.buffer.hooks.line.change_post(500)
         def handle_line_change(b, ln, v):
             if self.ln == ln.value and self.buffer == b:
                 self.correct_colpos()
 
         self.handle_line_change = handle_line_change
 
-        @core.buffer.hooks.line.insert_pre(500)
+        @core.buffer.hooks.line.insert_post(500)
         def handle_line_insert(b, ln):
-                if self.ln >= ln.value and self.buffer == b:
-                    self.ln += 1
+            if self.ln >= ln.value and self.buffer == b:
+                self.ln += 1
 
+                self.correct_linepos()
+                self.correct_colpos()
         self.handle_line_insert = handle_line_insert
 
     def move_cols(self, n):
@@ -51,85 +63,51 @@ class Point:
 
         self.correct_linepos()
 
-    def delete_forward(self, n):
-        while n > len(self.line) - self.cn:
-            n -= len(self.line)
-            n += self.cn
+    def merge_line_back(self):
+        if self.ln == 0:
+            return
 
-            l = self.line
+        oldline = self.line
 
-            if self.ln >= len(self.buffer) - 1:
-                break
+        self.buffer.delete(self.ln)
 
-            next = self.buffer[self.ln + 1]
-            l   += next
+        self.ln  -= 1
 
-            self.line = l
-            self.buffer.delete(self.ln + 1)
+        newline   = self.line
+        self.cn = len(newline)
+        newline  += oldline
+        self.line = newline
 
-        l = self.line
-        l.delete(self.ln, min(n, len(l) - self.ln))
-
-        self.line = l
+        return
 
     def delete_char(self):
         if not len(self.buffer):
             return
 
         if self.cn == 0:
-            if self.ln == 0:
-                return
-
-            l = self.line
-            self.buffer.delete(self.ln)
-            self.ln -= 1
-            prev     = self.line
-            prev    += l
-            self.line= prev
-
-            self.cn = len(self.line)
-
+            self.merge_line_back()
             return
 
-        
         self.cn -= 1
 
-        l = self.line
-        l.delete(self.cn, 1)
-        self.line = l
+        self.line = self.line[:-1]
+
+    def insert(self, string):
+        for char in string.encode("ascii"):
+            self.insert_char(char)
 
     def insert_char(self, char):
-        if not len(self.buffer):
-            self.buffer.insert(0)
-
         l = self.line
+
         l.insert(self.cn, char)
+
+
+        print(self.ln, "HIIIIII", file=sys.stderr)
         self.line = l
 
         self.cn += 1
 
-    def insert(self, string):
-        if not len(self.buffer):
-            self.buffer.insert(0)
-
-        start = self.line
-        start.delete(self.cn, len(start) - self.cn)
-
-        end   = self.line
-        end.delete(0, self.cn)
-
-        start += str2vec(string)
-        start += end
-
-        self.cn += len(string)
-
-        self.line = start
-
     def enter(self):
-        if not len(self.buffer):
-            self.buffer.insert(0)
-            return
-
         start = self.line
         start.delete(self.cn, len(start) - self.cn)
 
@@ -140,29 +118,23 @@ class Point:
 
         self.ln  += 1
         self.cn   = 0
+        print("BLEN", len(self.buffer), file=sys.stderr)
         self.buffer.insert(self.ln)
+        print("BLEN", len(self.buffer), file=sys.stderr)
+
         self.line = end
 
-
     def correct_colpos(self):
-        if len(self.buffer) == 0:
-            self.cn = 0
-            return
-
         linelen = len(self.buffer[self.ln])
 
-        if self.ln >= linelen:
-            self.ln = linelen - 1
+        if self.cn >= linelen:
+            self.cn = linelen
 
     def correct_linepos(self):
-        if len(self.buffer) == 0:
-            self.ln = 0
-            return
-
-        linelen = len(self.buffer[self.ln])
-
-        if self.ln >= linelen:
-            self.ln = linelen - 1
+        length = len(self.buffer)
+        print("LEN", length, file=sys.stderr)
+        if self.ln >= length:
+            self.ln = length - 1
 
     @property
     def line(self):
