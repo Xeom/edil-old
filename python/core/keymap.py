47de0101fa16cdc382b9b8315dff6f3c49bd8be2
Xeom <fwharf.fw@googlemail.com>
2016-04-23 02:21:11 +0100

    Added Key*Cont classes - Python keymaps are now a more self-contained system

--- python/core/keymap.py
+++ python/core/keymap.py
@@ -39,8 +39,8 @@ class KeymapFunct:
         pyfunct(pykeys)
 
 class Keymap:
-    def __init__(self):
-        self.struct = symbols.io.keymap.init()
+    def __init__(self, ptr):
+        self.struct = ptr
         self.functs = []
 
     def press(self, key):
@@ -63,3 +63,63 @@ class Keymap:
             return funct
 
         return rtn
+
+    def clear(self):
+        return symbols.io.keymap.clear(self.struct)
+
+class KeyFrame:
+    def __init__(self):
+        self.maps = []
+
+    def push(self, mapname, priority=0):
+        m = maps[mapname]
+
+        self.maps.append(m)
+
+    def press(self, key):
+        for map in self.maps:
+            if map.press(key) in (1, 2):
+                break
+
+    def clear(self):
+        for map in self.maps:
+            map.clear()
+
+class KeyCont:
+    def __init__(self):
+        self.items = {}
+
+    def __getitem__(self, name):
+        return self.items[name]
+
+class KeyFrameCont(KeyCont):
+    def __init__(self):
+        KeyCont.__init__(self)
+        self.active = None
+
+    def add(self, name):
+        if name not in self.items:
+            self.items[name] = KeyFrame()
+
+    def switch(self, name):
+        self.active = self.items[name]
+        self.active.clear()
+
+    def press(self, key):
+        if self.active != None:
+            self.active.press(key)
+
+class KeyMapCont(KeyCont):
+    def __init__(self):
+        self.items = {}
+
+    def add(self, name):
+        if name not in self.items:
+            self.items[name] = Keymap(symbols.io.keymap.init())
+
+frames = KeyFrameCont()
+maps   = KeyMapCont()
+
+@core.key.hooks.key(500)
+def handle_press(key):
+    frames.press(key)
