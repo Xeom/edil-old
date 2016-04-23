f8ec5ba9f4dc8ae939965af683a89630c00fa3c2
Xeom <fwharf.fw@googlemail.com>
2016-04-23 02:42:19 +0100

    Updated __init__.py to use new keymap api

--- python/__init__.py
+++ python/__init__.py
@@ -5,85 +5,23 @@ import core.windows
 import core.hook
 import core.key
 import core.keymap
+import core.table
 import ctypes
 import shared
 import symbols
 import signal
 
-import editor.uiupdates
-from editor.cursor.point import Point
-from core.key import Key
-from core.keymap import Keymap
-
-#@core.ui.hooks.win.frame.draw_pre(100)
-#def handle_frame_draw_pre(w):
-#    w.caption = str(len(w.buffer))
-#
-#@core.ui.hooks.win.content.draw_pre(100)
-#def handle_content_draw_pre(w, b):
-#    b.push("I was drawn :D")
-#
-#@core.windows.hooks.split(100)
-#def handle_split(wb, wbaby):
-#    b = wbaby.buffer
-#
-#@core.windows.hooks.create(100)
-#def handle_created(w):
-#    b = w.buffer
 symbols.hook.initsys()
-
 core.windows.initsys()
 core.ui.initsys()
 core.key.initsys()
 
 core.ui.refresh()
 
-@core.key.hooks.key(100)
-def handle_keypress(key):
-    km.press(key)
-    return
-    char = str(key)
-
-    if char == '^W':
-        core.windows.get_selected().split(core.windows.direction.up)
-
-    elif char == '^S':
-        core.windows.get_selected().split(core.windows.direction.down)
-
-    elif char == '^A':
-        core.windows.get_selected().split(core.windows.direction.left)
-
-    elif char == '^D':
-        core.windows.get_selected().split(core.windows.direction.right)
-
-    elif char == '^L':
-        core.windows.get_selected().buffer.read("__init__.py")
-
-    elif char == '^E':
-        core.windows.get_selected().next().select()
-
-    elif char == '^F':
-        core.windows.get_selected().delete()
-
-    elif char == '^X':
-        global alive
-        alive = False
-
-    elif char == '^U':
-        core.windows.get_selected().parent.select()
-
-    elif char == '^J':
-        p.enter()
-
-    elif char == 'BACKSPACE':
-        p.delete_char()
-
-    else:
-        p.insert(char)
-
-p  = Point(core.windows.get_selected().buffer)
-km = core.keymap.Keymap()
-km.struct
+import editor.uiupdates
+from editor.cursor.point import Point
+from core.key import Key
+from core.keymap import Keymap
 
 alive = True
 
@@ -91,26 +29,62 @@ def death_by_gentle_suffocation( signum, frame):
     global alive
     alive = False
 
-signal.signal(signal.SIGINT, death_by_gentle_suffocation)
+signal.signal(signal.SIGINT,  death_by_gentle_suffocation)
 signal.signal(signal.SIGTERM, death_by_gentle_suffocation)
 
-@km.add(Key("W", con=True), Key("UP"))
+p  = Point(core.windows.get_selected().buffer)
+
+################################################################################
+core.keymap.maps.add("win")
+
+winmap = core.keymap.maps["win"]
+
+@winmap.add(Key("UP"))
 def winsplitup(keys):
     core.windows.get_selected().split(core.windows.direction.up)
 
-@km.add(Key("W", con=True), Key("DOWN"))
+@winmap.add(Key("DOWN"))
 def winsplitdown(keys):
     core.windows.get_selected().split(core.windows.direction.down)
 
-@km.add(Key("W", con=True), Key("LEFT"))
+@winmap.add(Key("LEFT"))
 def winsplitleft(keys):
     core.windows.get_selected().split(core.windows.direction.left)
 
-@km.add(Key("W", con=True), Key("RIGHT"))
+@winmap.add(Key("RIGHT"))
 def winsplitright(keys):
     core.windows.get_selected().split(core.windows.direction.right)
 
-    
+@winmap.add(Key("c"))
+def windel(keys):
+    core.windows.get_selected().delete()
+
+@winmap.add(Key("e"))
+def winnext(keys):
+    core.windows.get_selected().next().select()
+
+core.keymap.maps.add("master")
+mastermap = core.keymap.maps["master"]
+
+@mastermap.add(Key("W", con=True))
+def masterwinon(keys):
+    core.keymap.frames.switch("winf")
+
+@mastermap.add(Key("w", esc=True))
+def masterwinoff(keys):
+    core.keymap.frames.switch("masterf")
+
+core.keymap.frames.add("masterf")
+core.keymap.frames.add("winf")
+core.keymap.frames["masterf"].push("master")
+core.keymap.frames["winf"].push("master")
+core.keymap.frames["winf"].push("win")
+core.keymap.frames.switch("masterf")
+
+
+
+#################################################################################
+
 while alive:
     char = symbols.lib.getch()
 
