45cf68765ad80aa7b53c9f02ef193e0bcc02c0b9
Xeom <fwharf.fw@googlemail.com>
2016-04-24 06:00:11 +0100

    added hashes_mem_long

--- include/container/hashes.h
+++ include/container/hashes.h
@@ -10,6 +10,8 @@ hash hashes_key_str(void *k);
 
 hash hashes_key_str_trans(void *k);
 
+hash hashes_mem_long(char *mem, size_t n);
+
 hash hashes_mem(char *mem, size_t n);
 
 #endif /* CONTAINER_HASHES_H */
