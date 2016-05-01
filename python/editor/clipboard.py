e54bd586cc5fdb785278d867aa09021931cba24f
Xeom <fwharf.fw@googlemail.com>
2016-05-01 22:05:31 +0100

    Added editor/clipboard.py Should have support for other clipboards added in the future. Also added hold_buffer to uiupdates for faster buffer operations.

---
+++ python/editor/clipboard.py
@@ -0,0 +1,21 @@
+import subprocess
+import cProfile
+
+import editor.uiupdates
+
+clipcmd = "xclip -selection clipboard -o -"
+
+def do_paste(cursor):
+    editor.uiupdates.hold_buffer(cursor.buffer)
+
+    clip = subprocess.check_output(clipcmd, shell=True)
+    clip = clip.decode("ascii")
+
+    split = clip.split("\n")
+    cursor.insert(split.pop(0))
+
+    for line in split:
+        cursor.enter()
+        cursor.insert(line)
+
+    editor.uiupdates.release_buffer()
