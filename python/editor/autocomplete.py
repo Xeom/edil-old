d2d611ef9bc4375b57938601fbf88c24dbd2a6e5
Xeom <fwharf.fw@googlemail.com>
2016-05-30 05:58:52 +0100

    Added commands, and autocomplete callbacks for them

---
+++ python/editor/autocomplete.py
@@ -0,0 +1,36 @@
+import os
+
+def number():
+    def rtn(string):
+        string = string.rstrip("0")
+
+        if not string:
+            string = "1"
+
+        return [string + "000" * n for n in (1, 2, 3, 0)]
+
+    return rtn
+
+def path(root):
+    root = os.path.abspath(
+        os.path.expanduser(root))
+
+    def rtn(string):
+        curpath = os.path.abspath(
+            os.path.join(root, string))
+
+        if os.path.isdir(curpath):
+            partial = ""
+
+        else:
+            curpath, partial = os.path.split(curpath)
+
+        subs = os.listdir(curpath)
+
+        subs = [os.path.join(curpath, sub) for sub in subs \
+                if sub.startswith(partial)]
+        subs = [os.path.relpath(sub, root) for sub in subs]
+
+        return subs
+
+    return rtn
---
+++ python/editor/command.py
@@ -0,0 +1,49 @@
+from core.hook import NativeHook
+import editor.query
+
+import weakref
+
+
+
+class CommandArg:
+    def __init__(self, type, prefix="", completecallback=None):
+        if prefix:
+            self.prefix  = prefix + ": "
+        else:
+            prefix = ""
+
+        self.complcb = completecallback
+        self.type    = type
+
+    def make_query(self, cb):
+        editor.query.make_query(cb, self.prefix, self.complcb)
+
+    def type_convert(self, string):
+        return self.type(string)
+
+class Command:
+    by_name = weakref.WeakValueDictionary()
+
+    def __init__(self, name, *args):
+        self.name = name
+        self.args = args
+        self.hook = NativeHook()
+
+        self.by_name[name] = self
+
+    def get_arg(self, args, n):
+        if n >= len(self.args):
+            self.hook.call(args)
+
+        else:
+            arg = self.args[n]
+
+            def cb(string):
+                args.append(arg.type_convert(string))
+                self.get_arg(args, n + 1)
+
+            arg.make_query(cb)
+
+
+    def run(self, keys=None):
+        self.get_arg([], 0)
