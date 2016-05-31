69c8ea621f9a12a1ac134f4a3fe63f4254f88ec3
Xeom <fwharf.fw@googlemail.com>
2016-05-31 06:51:20 +0100

    New unit_buffer tests

--- test/unit_buffer/sim.py
+++ test/unit_buffer/sim.py
@@ -15,6 +15,7 @@ def test_insert_end():
         b.insert(ind, None)
         print("buffer_set_line(b, ind, v) ->", 0)
         b[ind] = str(ind)
+        print("buffer_len(b) ->", len(b))
 
     test_print_buffer(b)
 
@@ -27,8 +28,54 @@ def test_insert_start():
         b.insert(0, None)
         print("buffer_set_line(b, 0, v) ->", 0)
         b[0] = str(ind)
+        print("buffer_len(b) ->", len(b))
+
+    test_print_buffer(b)
+
+def test_insert_middle():
+    b = []
+
+    for ind in range(TEST_LEN):
+        print("ind ->", ind)
+        print("buffer_insert(b, ind / 2) ->", 0)
+        b.insert(ind // 2, None)
+        print("buffer_set_line(b, ind / 2, v) ->", 0)
+        b[ind // 2] = str(ind)
+        print("buffer_len(b) ->", len(b))
+
+    test_print_buffer(b)
+
+def test_init():
+    print("b == NULL ->", 0)
+
+def test_del():
+    b = []
+
+    for ind in range(TEST_LEN):
+        b.insert(0, None)
+        b[0] = str(ind)
+
+    ind = 0
+    while ind < len(b):
+        if not ((ind // 20) % 2 and ind % 7 and ind % 11):
+            print("ind ->", ind)
+            print("buffer_delete(b, ind) ->", 0)
+            del b[ind]
+            print("buffer_len(b) ->", len(b))
+
+        ind += 1
+
+    test_print_buffer(b)
+
+    while len(b):
+        print("buffer_delete(b, 0) ->", 0)
+        del b[0]
+        print("buffer_len(b) ->",  len(b))
 
     test_print_buffer(b)
 
 test_insert_end()
 test_insert_start()
+test_insert_middle()
+test_init()
+test_del()
--- test/unit_buffer/test.c
+++ test/unit_buffer/test.c
@@ -10,9 +10,9 @@
 
 void test_insert_end(void);
 void test_insert_start(void);
+void test_insert_middle(void);
+void test_init(void);
 void test_del(void);
-void test_for(void);
-void test_find(void);
 
 static void test_print_buffer(buffer *b)
 {
@@ -58,6 +58,7 @@ void test_insert_end(void)
         TEST(ind, lu);
         TEST(buffer_insert(b, ind), d);
         TEST(buffer_set_line(b, ind, v), d);
+        TEST(buffer_len(b), lu);
     }
 
     test_print_buffer(b);
@@ -68,9 +69,9 @@ void test_insert_end(void)
 
 void test_insert_start(void)
 {
-    vec *v;
-    size_t ind;
+    size_t  ind;
     buffer *b;
+    vec    *v;
 
     b = buffer_init();
 
@@ -85,6 +86,98 @@ void test_insert_start(void)
         TEST(ind, lu);
         TEST(buffer_insert(b, 0), d);
         TEST(buffer_set_line(b, 0, v), d);
+        TEST(buffer_len(b), lu);
+    }
+
+    test_print_buffer(b);
+
+    buffer_free(b);
+    vec_free(v);
+}
+
+void test_insert_middle(void)
+{
+    vec    *v;
+    size_t  ind;
+    buffer *b;
+
+    b = buffer_init();
+
+    v = vec_init(10);
+    vec_insert(v, 0, 10, "          ");
+
+    PINFO("Inserting items...");
+
+    for (ind = 0; ind < TEST_LEN; ind++)
+    {
+        sprintf(vec_item(v, 0), "%lu", ind);
+        TEST(ind, lu);
+        TEST(buffer_insert(b,   ind / 2), d);
+        TEST(buffer_set_line(b, ind / 2, v), d);
+        TEST(buffer_len(b), lu);
+    }
+
+    test_print_buffer(b);
+
+    buffer_free(b);
+    vec_free(v);
+}
+
+void test_init(void)
+{
+    buffer *b;
+
+    PINFO("Initializing with buffer_init...");
+
+    b = buffer_init();
+    TEST(b == NULL, d);
+
+    buffer_free(b);
+}
+
+void test_del(void)
+{
+    vec   *v;
+    size_t ind;
+    buffer *b;
+
+    b = buffer_init();
+
+    v = vec_init(10);
+    vec_insert(v, 0, 10, "          ");
+
+    PINFO("Inserting items...");
+
+    for (ind = 0; ind < TEST_LEN; ind++)
+    {
+        sprintf(vec_item(v, 0), "%lu", ind);
+        buffer_insert(b, 0);
+        buffer_set_line(b, 0, v);
+    }
+
+    PINFO("Doing sparse delete...");
+
+    ind = 0;
+    while (ind < buffer_len(b))
+    {
+        if (!(ind / 20 % 2 && ind % 7 && ind % 11))
+        {
+            TEST(ind, lu);
+            TEST(buffer_delete(b, ind), d);
+            TEST(buffer_len(b),        lu);
+        }
+
+        ++ind;
+    }
+
+    test_print_buffer(b);
+
+    PINFO("Doing full delete...");
+
+    while (buffer_len(b))
+    {
+        TEST(buffer_delete(b, 0), d);
+        TEST(buffer_len(b),      lu);
     }
 
     test_print_buffer(b);
@@ -97,4 +190,7 @@ int main(void)
 {
     RUNTEST(insert_end);
     RUNTEST(insert_start);
+    RUNTEST(insert_middle);
+    RUNTEST(init);
+    RUNTEST(del);
 }
