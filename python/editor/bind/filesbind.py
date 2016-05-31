6261ee52e138f9ada9d1b4e6e1d127fb75bc7b33
Xeom <fwharf.fw@googlemail.com>
2016-05-31 23:53:08 +0100

    Converted filesbind to using Commands more

--- python/editor/bind/filesbind.py
+++ python/editor/bind/filesbind.py
@@ -3,6 +3,7 @@ import core.keymap
 
 from core.key import Key
 from editor.command import Command, CommandArg
+
 import editor.autocomplete
 
 mapname = "files-default"
@@ -12,29 +13,28 @@ filesmap = core.keymap.maps[mapname]
 
 modifier_key = Key("F", con=True)
 
-revert = Command("revert-buffer")
-
-@filesmap.add(modifier_key, Key("r"))
-def filesrevert(keys):
-    revert.run()
-
-@revert.hook(500)
+revert_cmd = Command("revert-buffer")
+@revert_cmd.hook(500)
 def revert_cb():
     editor.files.revert(
         core.windows.get_selected().buffer)
 
-associate = Command("associate-buffer",
-                    CommandArg(str, "File to associate",
-                               editor.autocomplete.path(".")))
+@filesmap.add(modifier_key, Key("r"))
+def revert_mapped(keys):
+    revert_cmd.run()
 
-@filesmap.add(modifier_key, Key("a"))
-def filesassociate(keys):
-    associate.run()
 
-@associate.hook(500)
+associate_cmd = Command("associate-buffer",
+                        CommandArg(str, "File to associate",
+                                   editor.autocomplete.path(".")))
+@associate_cmd.hook(500)
 def associate_cb(path):
     editor.files.associate(
         core.windows.get_selected().buffer, path)
+
+@filesmap.add(modifier_key, Key("a"))
+def associate_mapped(keys):
+    associate_cmd.run()
 
 dump = Command("dump-buffer")
 @filesmap.add(modifier_key, Key("d"))
