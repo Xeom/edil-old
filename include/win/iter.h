e6f8c964ebd3080aa49584058e5b2c02b1d5da1b
Xeom <fwharf.fw@googlemail.com>
2016-07-26 22:28:15 +0100

    Documented win/iter

--- include/win/iter.h
+++ include/win/iter.h
@@ -4,12 +4,56 @@
 
 # include "win/win.h"
 
+/*
+ * This function returns the first leaf subwindow of a window. If the window is
+ * a leaf, it returns the window itself. The first window is the sub1 of the
+ * sub1 of the sub1 ... of the window handed to the function. This makes it
+ * visually the top-left.
+ *
+ * @param w A pointer to a window to get the first subwindow of.
+ *
+ * @return  A pointer to the first subwindow.
+ *
+ */
 win *win_iter_first(win *w);
 
+/*
+ * This function returns the last leaf subwindow of a window. If the window is a
+ * leaf, it returns the window itself. The last window is the sub2 of the sub2
+ * of the sub2 ... of the window handed to the function. This makes it visually
+ * the bottom-right.
+ *
+ * @param w A pointer to a window to get the last subwindow of.
+ *
+ * @return  A pointer to the first subwindow.
+ *
+ */
 win *win_iter_last(win *w);
 
+/*
+ * Get the 'next' leaf window after a given one. This function, when called on
+ * its own results, will iterate over all windows. If you begin iterating at the
+ * win_iter_first of a window, all its subs will be iterated over, ending with
+ * its win_iter_last. This allows iteration over all the subs of a window. The
+ * windows kinda iterate from top-left to bottom-right. When the function is
+ * handed the win_iter_last of the root window, it returns the win_iter_first of
+ * the root.
+ *
+ * @param w A pointer to a window.
+ *
+ * @return  A pointer to the next window after the provided one.
+ *
+ */
 win *win_iter_next(win *w);
 
+/*
+ * The inverse of win_iter_next.
+ *
+ * @param w A pointer to a window.
+ *
+ * @return  A pointer to the previous window before the provided one.
+ *
+ */
 win *win_iter_prev(win *w);
 
 #endif /* WIN_ITER_H */
--- src/win/iter.c
+++ src/win/iter.c
@@ -4,6 +4,7 @@
 
 win *win_iter_first(win *w)
 {
+    /* Go down the sub1s for every window */
     while (win_issplit(w))
         w = w->cont.split.sub1;
 
@@ -12,6 +13,7 @@ win *win_iter_first(win *w)
 
 win *win_iter_last(win *w)
 {
+    /* Go down the sub2s for every window */
     while (win_issplit(w))
         w = w->cont.split.sub2;
 
@@ -20,17 +22,24 @@ win *win_iter_last(win *w)
 
 win *win_iter_next(win *w)
 {
+    /* It's best to imagine this as counting in binary. Since the windows are *
+     * a bitree, imagine every sub2 as a 1, and every sub1 as a 0.            */
+
+    /* Just like binary, get rid of the longest continuous line of trailing 1 */
     while (win_issub2(w))
         w = w->parent;
 
+    /* Find the sub2 sister of the first sub1 you find in the tree. */
     if (!win_isroot(w))
         w = w->parent->cont.split.sub2;
 
+    /* Get the first subwindow in it. */
     return win_iter_first(w);
 }
 
 win *win_iter_prev(win *w)
 {
+    /* An inverted version of win_iter_next */
     while (win_issub1(w))
         w = w->parent;
 
