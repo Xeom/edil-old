d02909ff6b131f124cea837b6b46763e4bd5749a
Xeom <fwharf.fw@googlemail.com>
2016-07-31 18:46:35 +0100

    Fleshed out files system with more error messages, tracking of open times

--- python/editor/files.py
+++ python/editor/files.py
@@ -1,13 +1,16 @@
 import os
 import editor.buffers.userlog
+import editor.query
+import time
 
-def revert(b):
-    #TODO: "Are you sure?"
-
+def revert(b, override=False):
     filename = b.properties["filename"]
+    modified = b.properties["modified"]
 
     if filename == None:
         return
+
+    filename = filename.decode("ascii")
 
     if not os.path.exists(filename):
         return
@@ -15,23 +18,81 @@ def revert(b):
     for index in reversed(range(len(b))):
         del b[index]
 
-    b.read(filename)
-    editor.buffers.userlog.log(b"Read file " + filename + b" to buffer")
+    def rlydoit():
+        b.read(filename)
+
+        b.properties["modified"]    = "False"
+        b.properties["revert-time"] = str(int(time.time()))
+
+        editor.buffers.userlog.log("Read file " + filename + " to buffer")
+
+    if override:
+        rlydoit()
+
+    else:
+        if modified and modified.lower() == "true":
+            editor.query.confirm(rlydoit, ("Buffer has been modified. "
+                                           "Really revert buffer?"))
+        else:
+            editor.query.confirm(rlydoit, "Really revert buffer?")
 
 def associate(b, filename):
-    curr = b.properties["filename"]
+    oldfilename = b.properties["filename"]
 
-    b.properties["filename"] = filename
-
-    if curr == None:
+    def rlydoit():
+        b.properties["filename"] = filename
+        editor.buffers.userlog.log("Associated buffer with file " + filename)
         revert(b)
 
-def dump(b):
+    editor.query.confirm(rlydoit, "Really associate with " + filename + "?")
+
+def dumpnew(b):
     filename = b.properties["filename"]
 
     if filename == None:
         return
 
-    b.dump(filename)
-    editor.buffers.userlog.log(b"Dumped buffer to file " + filename)
+    filename = filename.decode("ascii")
+
+    def rlydoit():
+        b.dump(filename)
+
+        b.properties["dump-time"] = str(int(time.time()))
+        b.properties["modified"]  = "False"
+
+        editor.buffers.userlog.log("Dumped buffer to new file " + filename)
+
+    editor.query.confirm(rlydoit, "Really dump to new file " + filename + "?")
+
+def dump(b):
+    filename    = b.properties["filename"]
+
+    if filename == None:
+        return
+
+    filename = filename.decode("ascii")
+
+    if not os.path.exists(filename):
+        dumpnew(b)
+        return
+
+    revert_time = b.properties["revert-time"]
+    revert_time = float(revert_time) if revert_time else time.time()
+    stats       = os.stat(filename)
+
+    def rlydoit():
+        b.dump(filename)
+
+        b.properties["dump-time"] = str(int(time.time()))
+        b.properties["modified"]  = "False"
+
+        editor.buffers.userlog.log("Dumped buffer to file " + filename)
+
+    if revert_time < stats.st_mtime:
+        editor.query.confirm(rlydoit, filename +
+                             (" has been changed since you "
+                              "began editing it. Really dump to it?"))
+
+    else:
+        editor.query.confirm(rlydoit, "Really dump to " + filename + "?")
 
