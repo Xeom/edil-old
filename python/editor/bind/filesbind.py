d96a0ca3492982f6eb37d2753d4df304d9737240
Francis Wharf <francis.wharf@quartix.net>
2016-08-25 13:08:09 +0100

    Replace remaining raw keymap binds with Command.map_to bindings.
    There were still functions bound to keymap hooks directly in
    python/editor/bind/cursorbind.py and python/editor/bind/filesbind.py. The only
    change in behavior this will produce, is that activating and deactivating the
    cursor will now print a statusbar message regardless of whether the command was
    run from a keypress, or other means.
    
    The affected commands were
     * cursor-activate
     * cursor-deactivate
     * file-revert
     * file-associate
     * file-dump

--- python/editor/bind/filesbind.py
+++ python/editor/bind/filesbind.py
@@ -13,17 +13,22 @@ filesmap = core.keymap.maps[mapname]
 
 modifier_key = Key("F", con=True)
 
+# file-revert
+#
+# Load or reload the file associated with the currently selected buffer from
+# disk.
 revert_cmd = Command("file-revert")
+
 @revert_cmd.hook(500)
 def revert_cb():
     editor.files.revert(
         core.windows.get_selected().buffer)
 
-@filesmap.add(modifier_key, Key("r"))
-def revert_mapped(keys):
-    revert_cmd.run()
+revert_cmd.map_to(filesmap, modifier_key, Key("r"))
 
-
+# file-associate
+#
+# Associate a particular file path with the currently selected buffer.
 associate_cmd = Command("file-associate",
                         CommandArg(str, "File to associate",
                                    editor.autocomplete.path(".")))
@@ -32,16 +37,17 @@ def associate_cb(path):
     editor.files.associate(
         core.windows.get_selected().buffer, path)
 
-@filesmap.add(modifier_key, Key("a"))
-def associate_mapped(keys):
-    associate_cmd.run()
+associate_cmd.map_to(filesmap, modifier_key, Key("a"))
 
-dump = Command("file-dump")
-@filesmap.add(modifier_key, Key("d"))
-def filesdump(keys):
-    dump.run()
+# file-dump
+#
+# Write the contents of the currently selected buffer to the file it is
+# associated with.
+dump_cmd = Command("file-dump")
 
-@dump.hook(500)
+@dump_cmd.hook(500)
 def dump_cb():
     editor.files.dump(
         core.windows.get_selected().buffer)
+
+dump_cmd.map_to(filesmap, modifier_key, Key("d"))
