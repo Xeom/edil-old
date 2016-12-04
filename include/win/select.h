a11304c5735bf5c925c4f06ce5833badf8c610e1
Xeom <fwharf.fw@googlemail.com>
2016-12-04 01:53:17 +0000

    Added documentation to win/select
    Just commented out the files some more. The single change to code
    is moving an && to the correct line.

--- include/win/select.h
+++ include/win/select.h
@@ -4,10 +4,26 @@
 
 # include "win/win.h"
 
+/* Hook called when a new window is selected.                              *
+ * Called with two parameters, the newly selected window, and the old one. */
 extern hook win_on_select;
 
+/*
+ * Select a window.
+ *
+ * @param w A pointer to the window to select.
+ *
+ * @return  0 on success, -1 on failure.
+ *
+ */
 int win_select_set(win *w);
 
+/*
+ * Get the currently selected window.
+ *
+ * @return A pointer to the window.
+ *
+ */
 win *win_select_get(void);
 
 #endif /* WIN_SELECT_H */
--- src/win/select.c
+++ src/win/select.c
@@ -4,22 +4,38 @@
 
 #include "win/select.h"
 
+/* Window selection is handled by each splitter window havng a selected       *
+ * property. This is set to one of sub1, sub2, and none. Sub1 and sub2        *
+ * indicate that either the first or second child are or contain the selected *
+ * window, while none indicates that the splitter window itself is selected.  *
+ *                                                                            *
+ * While this is clearly slower to set and get than just having a pointer to  *
+ * a selected window, it is kinda cuter, and also handles events like window  *
+ * deletion without adding extra code or hooks to other parts of the window   *
+ * system.                                                                    */
+
 hook_add(win_on_select, 2);
 
 int win_select_set(win *w)
 {
     win *oldselect;
 
+    /* Get the currently selected window to call win_on_select with */
     oldselect = win_select_get();
 
+    /* If the window we're now selecting is a splitter, tell it that *
+     * none of its children are selected.                            */
     if (win_issplit(w))
         w->cont.split.selected = none;
 
+    /* Iterate upwards from the newly selected window to the root */
     while (! win_isroot(w))
     {
         win *par;
+        /* Get the window's parent */
         par = w->parent;
 
+        /* Set the parent's selected child correctly. */
         if (win_issub1(w))
             par->cont.split.selected = sub1;
 
@@ -38,10 +54,13 @@ win *win_select_get(void)
 {
     win *w;
 
+    /* Start iterating from the root window */
     w = win_root;
 
-    while (win_issplit(w)
-           && w->cont.split.selected != none)
+    /* While the window is a splitter and not itself selected, *
+     * iterate to its selected child                           */
+    while (win_issplit(w) &&
+           w->cont.split.selected != none)
     {
         if (w->cont.split.selected == sub1)
             w = w->cont.split.sub1;
